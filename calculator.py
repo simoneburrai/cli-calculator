77388ff754229bb2ee738cd538e184c6e48be7be
simoneburrai <burrai.s98@gmail.com>
2025-11-18 21:37:27 +0100

    feat: calculator with four operations with input in python

---
+++ calculator.py
@@ -0,0 +1,20 @@
+from operations import sum, subctract, multiply, divide
+
+def calculation():
+    first_number = int(input("Inserisci un numero"))
+    while type(first_number) != "<class 'int'>":
+        first_number = input("Reinserici un valore valido ")
+
+    second_number = int(input("Inserisci un altro numero"))
+    operation = input("Inserisci un'operatore")
+    
+    if(operation == "+"):
+        return sum(first_number, second_number)
+    elif(operation == "-"):
+        return subctract(first_number, second_number)
+    elif(operation == "*"):
+        return multiply(first_number, second_number)
+    elif(operation == "/"):
+        return divide(first_number, second_number)
+    
+print("Il risultato è ", calculation())
---
+++ operations.py
@@ -0,0 +1,12 @@
+def sum (a,b):
+    return a + b
+
+def subctract (a,b): 
+    return a - b
+
+def multiply (a, b):
+    return a * b
+
+def divide (a, b):
+    return a / b
+
